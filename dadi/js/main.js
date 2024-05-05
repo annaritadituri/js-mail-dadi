f5ee7568e208ebdb60d411df11b0843832d02c7c
annaritadituri <annaritadituri@icloud.com>
2024-05-05 19:56:54 +0200

    Es dadi completed

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 //////////////////////////////////////////////////////////
+
+//GENERARE DUE NUMERI RANDOM DA ASSEGNARE UNO AL COMPUTER E L'ALTRO ALL'UTENTE
+let numeroComputer = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+console.log({numeroComputer});
+let numeroUtente = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+console.log({numeroUtente});
+
+//STABILIRE QUAL È IL NUMERO PIÙ ALTO
+if (numeroComputer > numeroUtente) {
+    console.log("Il computer ti ha battuto!");
+} else if (numeroUtente > numeroComputer) {
+    console.log("Hai vinto contro il computer!");
+} else {
+    console.log("Siete pari per questo giro...");
+}
